0fdeeb700654c1bdd8ef2a28a8ba47392323734e
JesseleDurana <jesseleadt@gmail.com>
2018-11-04 15:56:47 -0400

    Add fav icon to result item

--- src/components/molecules/ResultItem.js
+++ src/components/molecules/ResultItem.js
@@ -3,6 +3,8 @@ import Grid from "@material-ui/core/Grid";
 import TitleLink from "../atoms/TitleLink"
 import ResultLink from "../atoms/ResultLink"
 import ResultSnippet from "../atoms/ResultSnippet"
+import Favorite from '@material-ui/icons/Favorite'
+import IconButton from '@material-ui/core/IconButton'
 
 class ResultItem extends Component {
 
@@ -14,7 +16,11 @@ class ResultItem extends Component {
 				justify="flex-start"
 				alignItems="flex-start"
 			>
-				<TitleLink link={link} title={title}></TitleLink>
+				<div><TitleLink link={link} title={title}></TitleLink>
+					<IconButton color="inherit">
+						<Favorite color="disabled" />
+					</IconButton>
+				</div>
                 <ResultLink link={link}></ResultLink>
                 <ResultSnippet>{snippet}</ResultSnippet>
 			</Grid>
